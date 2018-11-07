302eb8ffe45e0050b950dfacc3e416dfb93b82aa
gitah223 <angelox223@yahoo.com>
2018-11-07 01:17:12 -0800

    Update index.js
    Added error handling and range for user input on HomeAssistQuestions intent

--- index.js
+++ index.js
@@ -49,29 +49,63 @@ const handlers = {
 
         
         
-        if(this.event.request.dialogState !== 'COMPLETED'){
-            this.emit(':delegate');
-        }
-        else{
-            const feelingScore = this.event.request.intent.slots.feelingRating.value;
-            const sleepingScore = this.event.request.intent.slots.sleepingRating.value;
-            const breathingScore = this.event.request.intent.slots.breathingRating.value;
-            const swollenScore = this.event.request.intent.slots.SwollenRating.value;
-            const id = this.event.request.intent.slots.id.value;
-
+            if(this.event.request.dialogState !== 'COMPLETED'){
+                    //ID
+            if (this.event.request.intent.slots.id.value == '?') {
+                let prompt = "Sorry I didn't hear your ID number";
+                let reprompt = "Can you give me your ID number";
+                this.emit(':elicitSlot', 'id', prompt, reprompt, this.event.request.intent); 
+            } 
+            //Feeling
+            else if (this.event.request.intent.slots.feelingRating.value == '?' || this.event.request.intent.slots.feelingRating.value < 1 || this.event.request.intent.slots.feelingRating.value > 10) { 
+                let prompt = "Sorry I didn't hear rating for how you feel or you didnt't give me a number from one to ten";
+                let reprompt = "Tell me how you feel from one to ten";
+                this.emit(':elicitSlot', 'feelingRating', prompt, reprompt); 
+            } 
+            //Sleeping
+            else if (this.event.request.intent.slots.sleepingRating.value == '?' || this.event.request.intent.slots.sleepingRating.value < 1 || this.event.request.intent.slots.sleepingRating.value > 10) {
+                let prompt = "Sorry I didn't hear your rating for your sleeping score or you didnt't give me a number from one to ten";
+                let reprompt = "Tell me how you feel from one to ten";
+                this.emit(':elicitSlot', 'sleepingRating', prompt, reprompt);
+            } 
+            //Breathing
+            else if (this.event.request.intent.slots.breathingRating.value == '?' || this.event.request.intent.slots.breathingRating.value < 1 || this.event.request.intent.slots.breathingRating.value > 10) {
+                let prompt = "Sorry I didn't hear your breathing rating or you didnt't give me a number from one to ten";
+                let reprompt = "Tell me how you feel from one to ten";
+                this.emit(':elicitSlot', 'breathingRating', prompt, reprompt); 
+            } 
+            //Swollen
+            else if (this.event.request.intent.slots.SwollenRating.value == '?' || this.event.request.intent.slots.SwollenRating.value < 1 || this.event.request.intent.slots.SwollenRating.value > 10) {
+                let prompt = "Sorry I didn't hear rating for your swollen rating or you didnt't give me a number from one to ten";
+                let reprompt = "Tell me how you feel from one to ten";
+                this.emit(':elicitSlot', 'SwollenRating', prompt, reprompt); 
+            }
+            else{
+                this.emit(':delegate');
+            }
             
-            this.attributes.healthscores.patientID = id;
-            this.attributes.healthscores.scores['feeling'].score = feelingScore;
-            this.attributes.healthscores.scores['sleeping'].score = sleepingScore;
-            this.attributes.healthscores.scores['breathing'].score = breathingScore;
-            this.attributes.healthscores.scores['swollen'].score = swollenScore;
-
-            //var request = require('request');
-            //request.post('http://cms3.dclhealth.com/alexa_get/alexa_get_insert.php?patient_id=' + id + '&response1=' + feelingScore + '&response2' + sleepingScore);
-            
-            this.response.speak("Health Scores Recorded");
-            this.emit(':responseReady');
-        }
+            }
+                //Complete
+                else {
+                const feelingScore = this.event.request.intent.slots.feelingRating.value;
+                const sleepingScore = this.event.request.intent.slots.sleepingRating.value;
+                const breathingScore = this.event.request.intent.slots.breathingRating.value;
+                const swollenScore = this.event.request.intent.slots.SwollenRating.value;
+                const id = this.event.request.intent.slots.id.value;
+    
+                
+                this.attributes.healthscores.patientID = id;
+                this.attributes.healthscores.scores['feeling'].score = feelingScore;
+                this.attributes.healthscores.scores['sleeping'].score = sleepingScore;
+                this.attributes.healthscores.scores['breathing'].score = breathingScore;
+                this.attributes.healthscores.scores['swollen'].score = swollenScore;
+    
+                //var request = require('request');
+                //request.post('http://cms3.dclhealth.com/alexa_get/alexa_get_insert.php?patient_id=' + id + '&response1=' + feelingScore + '&response2' + sleepingScore);
+                
+                this.response.speak("Health Scores Recorded");
+                this.emit(':responseReady');
+            }
 
 
     },
